531948c6babf7802ea1ecd9892807088e497e587
Shriraksha-Bhat <67705206+Shriraksha-Bhat@users.noreply.github.com>
2022-10-11 17:40:53 +0530

    Create About.php
    Make file About.php

---
+++ About.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>home</title>
+
+        <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link rel="stylesheet" href="CSS/Style.css">
+
+    </head>
+    <body>
+   
+      <section class="header">
+         <a href="home.php" class="logo">HOLIDAYZZZ</a>
+         <nav class="navbar">
+               <a href="home.php">HOME</a>
+               <a href="about.php">ABOUT</a>
+               <a href="package.php">PACKAGE</a>
+               <a href="book.php">BOOK</a>
+         </nav>
+         <div id="menu-btn" class="fas fa-bars"></div>
+      </section>
+
+      <div class="heading" style="background:url(images/aboutus.jpg) no-repeat">
+         <h1>about us</h1>
+      </div>
+
+      <section class="about">
+         <div class="image">
+            <img src="images/aboutus-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>why choose us?</h3>
+            <p>‘HOLIDAYZZZ’ “Travel is the main thing you purchase that makes you more extravagant”. We, at ‘HOLIDAYZZZ’, swear by this and put stock in satisfying travel dreams that make you perpetually rich constantly.</p>
+            <p>We have been moving excellent encounters for a considerable length of time through our cutting-edge planned occasion bundles and other fundamental travel administrations. We rouse our clients to carry on with a rich life, brimming with extraordinary travel encounters.</p>
+         </div>
+      </section>
+
+      <section class="reviews">
+         <h1 class="heading-title"> clients reviews </h1>
+         <div class="swiper reviews-slider">
+            <div class="swiper-wrapper">
+               <div class="swiper-slide slide">
+                  <p>Everything went off without a hitch in terms of the program! The meals are substantial, the services are of great quality, and the rooms are comfortable and clean. I strongly advise you to use this service since you will be able to go with peace of mind!</p>
+                  <h3>Shashi Deshpande</h3>
+               </div>
+               <div class="swiper-slide slide">
+                  <p>A huge thanks to the entire team; we know this vacation would not have been the same without 'Holidayzzz'. Exploring the city as it evolves in freedom, peace, and harmony is a breathtaking experience.</p>
+                  <h3>Geetha</h3>
+               </div>
+               <div class="swiper-slide slide">
+                  <p>A very trustworthy service and very accommodating to the customer requirements. Will always look forward for seeking this service. Thank you so much.</p>
+                  <h3>Bindu</h3>
+               </div>
+            </div>
+         </div>
+      </section>
+
+      <section class="footer">
+         <div class="box-container">
+            <div class="box">
+               <h3>quick links</h3>
+               <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+               <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+               <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+               <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+            </div>
+            <div class="box">
+               <h3>extra links</h3>
+               <a href="About.php"> <i class="fas fa-angle-right"></i> about us</a>
+               <a href="Privacy.php"> <i class="fas fa-angle-right"></i> privacy policy</a>
+               <a href="Terms.php"> <i class="fas fa-angle-right"></i> terms of use</a>
+           </div>    
+           <div class="box">
+               <h3>contact info</h3>
+               <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+               <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+               <a href="#"> <i class="fas fa-envelope"></i> Travel@gmail.com </a>
+               <a href="#"> <i class="fas fa-map"></i> Bangalore </a>
+            </div>     
+         </div>
+      </section>
+
+      <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+      <script src="js/script.js"></script>
+
+   </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
